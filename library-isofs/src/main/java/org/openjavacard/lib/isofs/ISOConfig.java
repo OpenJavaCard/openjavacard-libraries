2944c302389ab59ccb0f2290f307eb807dd38af9
Ingo Albrecht <commit@promovicz.org>
2020-05-15 02:36:06 +0200

    isofs: Comments for config options

--- library-isofs/src/main/java/org/openjavacard/lib/isofs/ISOConfig.java
+++ library-isofs/src/main/java/org/openjavacard/lib/isofs/ISOConfig.java
@@ -2,10 +2,24 @@ package org.openjavacard.lib.isofs;
 
 public interface ISOConfig {
 
+    /**
+     * Maximum number of children in a DF
+     */
     short DF_SIZE = 16;
 
+    /**
+     * Maximum length of FCI/FCP/FMD records
+     */
     short BER_MAX_LENGTH = (short)128;
+
+    /**
+     * Maximum number of tags in TLV output
+     */
     byte  BER_MAX_TAGS = (byte)16;
+
+    /**
+     * Maximum depth of tags in TLV output
+     */
     byte  BER_MAX_DEPTH = (byte)4;
 
 }
