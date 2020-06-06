d42f9496400676c8578997b49010179f87140dc3
Ingo Albrecht <commit@promovicz.org>
2020-06-06 08:07:55 +0200

    Added cvm library
    Can't build it yet due to ant-javacard limitations or the limitations of
    my knowledge in using it.

---
+++ library-cvm/src/main/java/org/openjavacard/lib/cvm/CVMPIN.java
@@ -0,0 +1,53 @@
+package org.openjavacard.lib.cvm;
+
+import javacard.framework.PIN;
+import javacard.framework.Shareable;
+import org.globalplatform.CVM;
+import org.globalplatform.GPSystem;
+
+/**
+ * Wraps a CVM for use as an OwnerPIN
+ */
+public class CVMPIN implements PIN, Shareable {
+
+    /** The CVM that we wrap */
+    private CVM mCVM;
+
+    /**
+     * Get a CVMPIN representing a system CVM
+     *
+     * @param cvmIdentifier to look for
+     * @return resulting wrapper
+     */
+    public static CVMPIN get(byte cvmIdentifier) {
+        CVM cvm = GPSystem.getCVM(cvmIdentifier);
+        return new CVMPIN(cvm);
+    }
+
+    /**
+     * Construct a CVM wrapper
+     *
+     * @param cvm to wrap
+     */
+    public CVMPIN(CVM cvm) {
+        mCVM = cvm;
+    }
+
+    public byte getTriesRemaining() {
+        return mCVM.getTriesRemaining();
+    }
+
+    public boolean isValidated() {
+        return mCVM.isVerified();
+    }
+
+    public boolean check(byte[] buf, short off, byte len) throws ArrayIndexOutOfBoundsException, NullPointerException {
+        short res = mCVM.verify(buf, off, len, CVM.FORMAT_ASCII);
+        return res == CVM.CVM_SUCCESS;
+    }
+
+    public void reset() {
+        mCVM.resetAndUnblockState();
+    }
+
+}
